e1a935418612fc93fff5447ec5bb8b67b98c8cd1
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-19 11:41:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,37 +1,34 @@
-
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import csv
 import psycopg2
 from sqlalchemy import create_engine
-url = 'https://screener.in/company/RELIANCE/consolidated/' 
+
+url = 'https://screener.in/company/RELIANCE/consolidated/'
 webpage = requests.get(url)
 soup = bs(webpage.text,'html.parser')
-data = soup.find('section', id="profit-loss", class_="card card-large")
+
+data = soup.find('section', id="profit-loss")
 tdata= data.find("table")
 
 table_data = []
- 
+
 for row in tdata.find_all('tr'):
     row_data = []
     for cell in row.find_all(['th','td']):
         row_data.append(cell.text.strip())
     table_data.append(row_data)
 
-with open("table_data.csv", 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(table_data)
-
 
 df_table = pd.DataFrame(table_data)
 df_table.iloc[0,0] = 'Section'
 df_table.columns = df_table.iloc[0]
-df_table = df_table[1:]
-print(df_table)
+df_table = df_table.iloc[1:,:-1]
+for i in df_table.iloc[:,1:].columns:
+    df_table[i] = df_table[i].str.replace(',','').str.replace('%','').apply(eval)
 
 
-db_host = "172.27.80.1"
+db_host = "192.168.1.223" #"192.168.29.101"
 db_name = "exampledb"
 db_user = "docker"
 db_password = "docker"
@@ -39,23 +36,6 @@ db_port = "5432"
 
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-# Load the DataFrame into the PostgreSQL database
-df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=False)
+df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=True)
 
-print("Data loaded successfully into PostgreSQL database!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Data loaded to PostgreSQL")
