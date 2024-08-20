ab6466f0176514553e04aa35021286f87fe1b947
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-20 15:10:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,29 +4,42 @@ import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine
 
+# URL of the webpage
 url = 'https://screener.in/company/RELIANCE/consolidated/'
+
+# Fetch the webpage
 webpage = requests.get(url)
-soup = bs(webpage.text,'html.parser')
 
+# Parse the HTML content
+soup = bs(webpage.text, 'html.parser')
+
+# Find the profit and loss section
 data = soup.find('section', id="profit-loss")
-tdata= data.find("table")
 
+# Find the table within the section
+tdata = data.find("table")
+
+# Extract table data
 table_data = []
-
 for row in tdata.find_all('tr'):
     row_data = []
-    for cell in row.find_all(['th','td']):
+    for cell in row.find_all(['th', 'td']):
         row_data.append(cell.text.strip())
     table_data.append(row_data)
 
-
+# Create a DataFrame from the table data
 df_table = pd.DataFrame(table_data)
-df_table.iloc[0,0] = 'Section'
-df_table.columns = df_table.iloc[0]
-df_table = df_table.iloc[1:,:-1]
-for i in df_table.iloc[:,1:].columns:
-    df_table[i] = df_table[i].str.replace(',','').str.replace('%','').apply(eval)
 
+# Set the first row as the header and drop it from the DataFrame
+df_table.iloc[0, 0] = 'Section'
+df_table.columns = df_table.iloc[0]
+df_table = df_table.iloc[1:, :-1]
+
+# Clean and transform the data
+for i in df_table.iloc[:, 1:].columns:
+    df_table[i] = df_table[i].str.replace(',', '').str.replace('%', '').apply(eval)
+
+# Transpose the DataFrame
 df_table_transposed = df_table.T
 df_table_transposed.columns = df_table_transposed.iloc[0]
 df_table_transposed = df_table_transposed[1:]
@@ -37,8 +50,15 @@ db_user = "docker"
 db_password = "docker"
 db_port = "5432"
 
+# Create the database engine
 engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
 
-df_table.to_sql('profit_loss_data', engine, if_exists='replace', index=True)
+# Load the transposed DataFrame into the PostgreSQL database
+df_table_transposed.to_sql('profit_loss_data_transposed', engine, if_exists='replace', index=True)
 
-print("Data loaded to PostgreSQL")
+print("Data loaded successfully into PostgreSQL database!")
+ 
+
+
+
+
