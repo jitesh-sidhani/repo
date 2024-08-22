8bba5eb9486e50789c6b1138fced0b11ffe04a9a
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-22 18:07:49 +0530

    Create script3.py

---
+++ script3.py
@@ -0,0 +1,16 @@
+import requests
+
+import os
+
+stock_code = os.environ.get('STOCK_CODE')
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
+
+
+
+
+
+print("Data loaded successfully into PostgreSQL database!")
+ 
+
+
