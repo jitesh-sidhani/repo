50d929f62e5f60c05ef55aaea228ebca099fdb83
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-27 16:09:04 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -9,6 +9,7 @@ import time
 import os
 import pandas as pd
 from openpyxl import load_workbook
+from sqlalchemy import create_engine
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")  # Run in headless mode
