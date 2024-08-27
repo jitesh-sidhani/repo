6d44a43361f18a55cae851a599576613bfb76389
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-27 17:26:40 +0530

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -88,9 +88,9 @@ print("Files in download directory before wait:", os.listdir(download_dir))
 # # Print resulting DataFrame
 # print(df)
 
+col=["Mar-13"	,"Mar-14","Mar-15"	,"Mar-16"	,"Mar-17"	,"Mar-18"	,"Mar-19"	,"Mar-20"	,"Mar-21","Mar-22","Mar-23"	,"Mar-24"			]
 
-
-df = pd.read_excel("Reliance Industr.xlsx", sheet_name="Data Sheet",skiprows=1)
+df = pd.read_excel("Reliance Industr.xlsx", sheet_name="Data Sheet",skiprows=1,columns=col)
 
 # Print column names
 print(df.columns)
