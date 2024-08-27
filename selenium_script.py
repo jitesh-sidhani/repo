fa81d0ffefc61d0a1d368cb65b2b461b20cff0a3
JITESH SIDHANI <110370070+jitesh-sidhani@users.noreply.github.com>
2024-08-27 17:20:26 +0530

    Update selenium_script.py

--- selenium_script.py
+++ selenium_script.py
@@ -90,7 +90,7 @@ print("Files in download directory before wait:", os.listdir(download_dir))
 
 
 
-df = pd.read_excel("Reliance Industr.xlsx", sheet_name="Data Sheet")
+df = pd.read_excel("Reliance Industr.xlsx", sheet_name="Data Sheet",skiprows=1)
 
 # Print column names
 print(df.columns)
